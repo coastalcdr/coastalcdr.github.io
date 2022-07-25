fbb3542a8e2242a0c63e4e063909b8fe2ee1713a
coastalcdr <105960308+coastalcdr@users.noreply.github.com>
2022-07-24 23:17:29 -0500

    Updated and added assets

---
+++ init.js
@@ -0,0 +1,14 @@
+
+(function($){
+    $(function(){
+        $('.tabs').tabs();
+        $('.sidenav').sidenav();
+        $('.parallax').parallax();
+        $('.materialboxed').materialbox();
+        $('.datepicker').datepicker();
+        $('.carousel').carousel();
+        $('.collapsible').collapsible();
+    }); 
+  })(jQuery); 
+
+
